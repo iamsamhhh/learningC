bbbc51367b95123be35bc8e0da481e0ad5000831
iamsamhhh <sc_samchen@icloud.com>
2021-04-08 22:00:24 +0800

    add swap

--- Menu/Menu/main.c
+++ Menu/Menu/main.c
@@ -12,7 +12,8 @@ int GetInt(void);
 char GetFirst(void);
 char GetChoice(void);
 void Count(void);
-
+void Swap(int *, int *);
+void Swap_Main(void);
 
 int main(void) {
     int choice;
@@ -27,6 +28,9 @@ int main(void) {
                 break;
             case 'c':
                 Count();
+                break;
+            case 's':
+                Swap_Main();
                 break;
             default:
                 printf("program error!\n");
@@ -71,10 +75,11 @@ char GetChoice(void){
     
     printf("Enter the letter of your choice:\n");
     printf("a. Advice        b. Bell\n"
-           "c. Count         q. Quit\n");
+           "c. Count         s. Swap\n"
+           "q. Quit\n");
     ch = GetFirst();
-    while ((ch < 'a' || ch > 'c') && ch != 'q') {
-        printf("Please responde with a, b, c or q.\n");
+    while (((ch < 'a' || ch > 'c') && ch != 'q') && ch != 's') {
+        printf("Please responde with a, b, c, s, or q.\n");
         ch = GetFirst();
     }
     
@@ -90,5 +95,32 @@ void Count(void){
     n = GetInt();
     for (i = 1; i <= n; i++)
         printf("%d\n", i);
+    
+}
+
+void Swap(int * a, int * b){ // 声明指针
+    printf("Before swap a = %3d, b = %3d\n"
+           "And the address of a is %p, for b is %p\n",
+           *a, *b, // 解指针
+           a, b); // 指针
+    int temp = *a; // 解指针
+    *a = *b;
+    *b = temp;
+    
+}
+
+void Swap_Main(){
+    int a, b;
+    printf("enter a number for a: ");
+    a = GetInt();
+    printf("and then b: ");
+    b = GetInt();
+    
+    Swap(&a, &b); // 把a和b的地址赋值给指针
+    
+    printf("After swap a = %4d, b = %4d\n"
+           "And the address of a is %p, for b is %p\n",
+           a, b,
+           &a, &b); //获取a和b的地址
     
 }
